65c59f01f374ad23543662a2347af0b735c3a269
cuddiale <alessiocuddretto@hotmail.com>
2024-06-21 10:00:17 +0200

    update

--- script.js
+++ script.js
@@ -6,4 +6,12 @@ const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 
 const postsContainer = document.getElementById("posts-container");
 
-const fetchData = async () => {};
+const fetchData = async () => {
+  try {
+    const res = await fetch(forumLatest);
+    const data = await res.json();
+    console.log(data);
+  } catch (err) {}
+};
+
+fetchData();
