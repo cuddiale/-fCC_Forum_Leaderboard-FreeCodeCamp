da5ce4d3442335f09e1ef8b3a18e0007f6ffedf8
cuddiale <alessiocuddretto@hotmail.com>
2024-06-24 09:30:49 +0200

    Update

--- script.js
+++ script.js
@@ -22,18 +22,31 @@ const showLatestPosts = (data) => {
   const { topic_list, users } = data;
   const { topics } = topic_list;
 
-  postsContainer.innerHTML = topics.map((item) => {
-    const {
-      id,
-      title,
-      views,
-      posts_count,
-      slug,
-      posters,
-      category_id,
-      bumped_at,
-    } = item;
+  postsContainer.innerHTML = topics
+    .map((item) => {
+      const {
+        id,
+        title,
+        views,
+        posts_count,
+        slug,
+        posters,
+        category_id,
+        bumped_at,
+      } = item;
 
-    return `<tr></tr>`;
-  });
+      return `
+    <tr>
+      <td>
+        <p class="post-title">${title}</p>
+      </td>
+      <td></td>
+      <td>
+      
+      </td>
+      <td></td>
+      <td></td>
+    </tr>`;
+    })
+    .join("");
 };
