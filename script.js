2ef224cdc3edd85b588bb66ddf21441da294fcc9
cuddiale <alessiocuddretto@hotmail.com>
2024-06-25 09:37:00 +0200

    Update

--- script.js
+++ script.js
@@ -6,9 +6,55 @@ const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 
 const postsContainer = document.getElementById("posts-container");
 
+const allCategories = {
+  299: { category: "Career Advice", className: "career" },
+  409: { category: "Project Feedback", className: "feedback" },
+  417: { category: "freeCodeCamp Support", className: "support" },
+  421: { category: "JavaScript", className: "javascript" },
+  423: { category: "HTML - CSS", className: "html-css" },
+  424: { category: "Python", className: "python" },
+  432: { category: "You Can Do This!", className: "motivation" },
+  560: { category: "Backend Development", className: "backend" },
+};
+
+const forumCategory = (id) => {
+  let selectedCategory = {};
+
+  if (allCategories.hasOwnProperty(id)) {
+    const { className, category } = allCategories[id];
+  }
+};
+
 const timeAgo = (time) => {
   const currentTime = new Date();
   const lastPost = new Date(time);
+
+  const timeDifference = currentTime - lastPost;
+  const msPerMinute = 1000 * 60;
+
+  const minutesAgo = Math.floor(timeDifference / msPerMinute);
+  const hoursAgo = Math.floor(minutesAgo / 60);
+  const daysAgo = Math.floor(hoursAgo / 24);
+
+  if (minutesAgo < 60) {
+    return `${minutesAgo}m ago`;
+  }
+
+  if (hoursAgo < 24) {
+    return `${hoursAgo}h ago`;
+  }
+
+  return `${daysAgo}d ago`;
+};
+
+const viewCount = (views) => {
+  const thousands = Math.floor(views / 1000);
+
+  if (views >= 1000) {
+    return `${thousands}k`;
+  }
+
+  return views;
 };
 
 const fetchData = async () => {
@@ -29,16 +75,8 @@ const showLatestPosts = (data) => {
 
   postsContainer.innerHTML = topics
     .map((item) => {
-      const {
-        id,
-        title,
-        views,
-        posts_count,
-        slug,
-        posters,
-        category_id,
-        bumped_at,
-      } = item;
+      const { id, title, views, posts_count, slug, posters, _id, bumped_at } =
+        item;
 
       return `
     <tr>
@@ -47,8 +85,8 @@ const showLatestPosts = (data) => {
       </td>
       <td></td>
       <td>${posts_count - 1}</td>
-      <td>${views}</td>
-      <td></td>
+      <td>${viewCount(views)}</td>
+      <td>${timeAgo(bumped_at)}</td>
     </tr>`;
     })
     .join("");
