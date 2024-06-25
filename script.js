32b45003b1236588d614d2eaf70687e3d8499d04
cuddiale <alessiocuddretto@hotmail.com>
2024-06-25 09:34:50 +0200

    Update

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 
 const postsContainer = document.getElementById("posts-container");
 
+const timeAgo = (time) => {
+  const currentTime = new Date();
+  const lastPost = new Date(time);
+};
+
 const fetchData = async () => {
   try {
     const res = await fetch(forumLatest);
@@ -41,10 +46,8 @@ const showLatestPosts = (data) => {
         <p class="post-title">${title}</p>
       </td>
       <td></td>
-      <td>
-      
-      </td>
-      <td></td>
+      <td>${posts_count - 1}</td>
+      <td>${views}</td>
       <td></td>
     </tr>`;
     })
